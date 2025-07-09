ec3d17d6dccc6ae0bdf3a87bfa51fcd077f1e75a
ChristinaKarlovich <christina.karlovich@gmail.com>
2025-07-09 22:38:02 +0300

    feat: install prettier

--- my-app/eslint.config.js
+++ my-app/eslint.config.js
@@ -1,18 +1,18 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
-import pluginReact from 'eslint-plugin-react';
-import { defineConfig } from 'eslint/config';
-import React from 'react';
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+import React from "react";
 
 export default defineConfig([
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     plugins: { js, React },
-    extends: ['js/recommended'],
+    extends: ["js/recommended"],
   },
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     languageOptions: {
       parserOptions: {
         ecmaFeatures: {
