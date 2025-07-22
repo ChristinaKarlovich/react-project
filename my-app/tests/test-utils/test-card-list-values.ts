bfccfdede3766812dfad3ad5496c420c5b356562
ChristinaKarlovich <christina.karlovich@gmail.com>
2025-07-23 00:41:30 +0300

    feat: add Card List Panel tests

---
+++ my-app/tests/test-utils/test-card-list-values.ts
@@ -0,0 +1,18 @@
+const testValues = [
+  {
+    uid: "1",
+    title: "Title 1",
+    season: { title: "Season 1" },
+    seasonNumber: "1",
+    episodeNumber: "1",
+  },
+  {
+    uid: "2",
+    title: "Title 2",
+    season: { title: "Season 2" },
+    seasonNumber: "2",
+    episodeNumber: "2",
+  },
+];
+
+export default testValues;
