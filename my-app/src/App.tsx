16aa4a10e69e136a1a38815aaac66a25b93eae59
ChristinaKarlovich <christina.karlovich@gmail.com>
2025-07-26 22:02:58 +0300

    feat: migrate App class component to function component

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,17 +1,14 @@
-import React from "react";
 import "./App.css";
 import Header from "./Components/Header/Header.tsx";
 import Main from "./Components/Main/Main.tsx";
 
-class App extends React.Component {
-  render(): React.ReactNode {
-    return (
-      <>
-        <Header />
-        <Main />
-      </>
-    );
-  }
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 }
 
 export default App;
--- my-app/src/Components/Main/Main.tsx
+++ my-app/src/Components/Main/Main.tsx
@@ -6,52 +6,52 @@ import CardSkeleton from "./CardListPanel/CardListItem/CardSkeleton";
 import ErrorInfo from "./ErrorInfo/ErrorInfo";
 import fetchData from "../../API/api";
 
-function Main () {
+function Main() {
   const lsText = localStorage.getItem("searchText");
   const [searchText, setSearchText] = useState(lsText);
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
 
-  useEffect(()=> {
+  useEffect(() => {
     loadData();
-  },[])
+  }, []);
 
-  async function buttonClick(text: string | null){
+  async function buttonClick(text: string | null) {
     localStorage.setItem("searchText", text ? text : "");
     setSearchText(text as string);
     setIsLoading(true);
     const res = await fetchData(text);
     setData(res);
-    setIsLoading(false)
-  };
+    setIsLoading(false);
+  }
 
   async function loadData() {
-    setIsLoading(true)
+    setIsLoading(true);
     const res = await fetchData(searchText);
     setData(res);
-    setIsLoading(false)
+    setIsLoading(false);
   }
 
   function errorButtonClicked() {
-    setError("error button clicked" );
-  };
-    if (error) {
-      throw Error(error);
-    }
-    let show: React.ReactElement | null = null;
-
-    if (isLoading) show = <CardSkeleton amount={10} />;
-    else if (error) show = <ErrorInfo message={error} />;
-    else show = <CardListPanel data={data} />;
-
-    return (
-      <div>
-        <SearchPannel showResult={buttonClick} />
-        {show}
-        <button onClick={errorButtonClicked}>Throw Error</button>
-      </div>
-    );
+    setError("error button clicked");
   }
+  if (error) {
+    throw Error(error);
+  }
+  let show: React.ReactElement | null = null;
+
+  if (isLoading) show = <CardSkeleton amount={10} />;
+  else if (error) show = <ErrorInfo message={error} />;
+  else show = <CardListPanel data={data} />;
+
+  return (
+    <div>
+      <SearchPannel showResult={buttonClick} />
+      {show}
+      <button onClick={errorButtonClicked}>Throw Error</button>
+    </div>
+  );
+}
 
 export default Main;
